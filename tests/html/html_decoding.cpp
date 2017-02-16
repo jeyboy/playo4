35be60ce0c7d012d468c71342e9478acaebc2787
JB <jeyboy1985@gmail.com>
2017-02-16 02:17:09 +0200

    update html decodings

--- tests/html/html_decoding.cpp
+++ tests/html/html_decoding.cpp
@@ -4,6 +4,8 @@
 //#include <qregularexpression.h>
 #include <qdebug.h>
 
+//    ∀ 	для всех 	&forall; 	&#8704;
+
 QHash<QByteArray, int> HtmlDecoding::html_entities = {
     { QByteArrayLiteral("quot"), 34 },
     { QByteArrayLiteral("amp"), 38 },
@@ -108,179 +110,177 @@ QHash<QByteArray, int> HtmlDecoding::html_entities = {
     { QByteArrayLiteral("thorn"), 254 },
     { QByteArrayLiteral("yuml"), 255 },
 
+    { QByteArrayLiteral("OElig"), 338 },
+    { QByteArrayLiteral("oelig"), 339 },
+
+    { QByteArrayLiteral("Scaron"), 352 },
+    { QByteArrayLiteral("scaron"), 353 },
+
+    { QByteArrayLiteral("Yuml"), 376 },
+
     { QByteArrayLiteral("fnof"), 402 },
 
-//    Греческие
-//    Α 	греческая заглавная альфа 	&Alpha; 	&#913;
-//    Β 	греческая заглавная бета 	&Beta; 	&#914;
-//    Γ 	греческая заглавная гамма 	&Gamma; 	&#915;
-//    Δ 	греческая заглавная дельта 	&Delta; 	&#916;
-//    Ε 	греческая заглавная эпсилон 	&Epsilon; 	&#917;
-//    Ζ 	греческая заглавная дзета 	&Zeta; 	&#918;
-//    Η 	греческая заглавная эта 	&Eta; 	&#919;
-//    Θ 	греческая заглавная тета 	&Theta; 	&#920;
-//    Ι 	греческая заглавная йота 	&Iota; 	&#921;
-//    Κ 	греческая заглавная каппа 	&Kappa; 	&#922;
-//    Λ 	греческая заглавная лямбда 	&Lambda; 	&#923;
-//    Μ 	греческая заглавная мю 	&Mu; 	&#924;
-//    Ν 	греческая заглавная ню 	&Nu; 	&#925;
-//    Ξ 	греческая заглавная кси 	&Xi; 	&#926;
-//    Ο 	греческая заглавная омикрон 	&Omicron; 	&#927;
-//    Π 	греческая заглавная пи 	&Pi; 	&#928;
-//    Ρ 	греческая заглавная ро 	&Rho; 	&#929;
-//    Σ 	греческая заглавная сигма 	&Sigma; 	&#931;
-//    Τ 	греческая заглавная тау 	&Tau; 	&#932;
-//    Υ 	греческая заглавная ипсилон 	&Upsilon; 	&#933;
-//    Φ 	греческая заглавная фи 	&Phi; 	&#934;
-//    Χ 	греческая заглавная хи 	&Chi; 	&#935;
-//    Ψ 	греческая заглавная пси 	&Psi; 	&#936;
-//    Ω 	греческая заглавная омега 	&Omega; 	&#937;
-//    α 	греческая строчная альфа 	&alpha; 	&#945;
-//    β 	греческая строчная бета 	&beta; 	&#946;
-//    γ 	греческая строчная гамма 	&gamma; 	&#947;
-//    δ 	греческая строчная дельта 	&delta; 	&#948;
-//    ε 	греческая строчная эпсилон 	&epsilon; 	&#949;
-//    ζ 	греческая строчная дзета 	&zeta; 	&#950;
-//    η 	греческая строчная эта 	&eta; 	&#951;
-//    θ 	греческая строчная тета 	&theta; 	&#952;
-//    ι 	греческая строчная йота 	&iota; 	&#953;
-//    κ 	греческая строчная каппа 	&kappa; 	&#954;
-//    λ 	греческая строчная ламбда 	&lambda; 	&#955;
-//    μ 	греческая строчная мю 	&mu; 	&#956;
-//    ν 	греческая строчная ню 	&nu; 	&#957;
-//    ξ 	греческая строчная кси 	&xi; 	&#958;
-//    ο 	греческая строчная омикрон 	&omicron; 	&#959;
-//    π 	греческая строчная пи 	&pi; 	&#960;
-//    ρ 	греческая строчная ро 	&rho; 	&#961;
-//    ς 	греческая конечная сигма 	&sigmaf; 	&#962;
-//    σ 	греческая строчная сигма 	&sigma; 	&#963;
-//    τ 	греческая строчная тау 	&tau; 	&#964;
-//    υ 	греческая строчная ипсилон 	&upsilon; 	&#965;
-//    φ 	греческая строчная фи 	&phi; 	&#966;
-//    χ 	греческая строчная хи 	&chi; 	&#967;
-//    ψ 	греческая строчная пси 	&psi; 	&#968;
-//    ω 	греческая строчная омега 	&omega; 	&#969;
-//    ϑ 	греческая символьная тета 	&thetasym; 	&#977;
-//    ϒ 	греческий ипсилон с хвостиком 	&upsih; 	&#978;
-//    ϖ 	греческая строчная пи (другой вариант) 	&piv; 	&#982;
-//    Основная пунктуация
-//    • 	маркер списка (буллит) 	&bull; 	&#8226;
-//    … 	горизонтальное многоточие 	&hellip; 	&#8230;
-//    ′ 	штрих 	&prime; 	&#8242;
-//    ″ 	двойной штрих 	&Prime; 	&#8243;
-//    ‾ 	верхняя черта 	&oline; 	&#8254;
-//    ⁄ 	косая черта 	&frasl; 	&#8260;
-//    Буквоподобные символы
-//    ℘ 	рукописная заглавная P = power set = Weierstrass p 	&weierp; 	&#8472;
-//    ℑ 	чёрная заглавная I = мнимая часть 	&image; 	&#8465;
-//    ℜ 	чёрная заглавная R = вещественная часть 	&real; 	&#8476;
-//    ™ 	товарный знак 	&trade; 	&#8482;
-//    ℵ 	буква Алеф — кардинальное число 	&alefsym; 	&#8501;
-//    Стрелки
-//    ← 	влево 	&larr; 	&#8592;
-//    ↑ 	вверх 	&uarr; 	&#8593;
-//    → 	вправо 	&rarr; 	&#8594;
-//    ↓ 	вниз 	&darr; 	&#8595;
-//    ◄ 	влево 		&#9668;
-//    ▲ 	вверх 		&#9650;
-//    ► 	вправо 		&#9658;
-//    ▼ 	вниз 		&#9660;
-//    ↔ 	влево-вправо 	&harr; 	&#8596;
-//    ↵ 	вниз с углом вправо = возврат каретки 	&crarr; 	&#8629;
-//    ⇐ 	двойная стрелка влево 	&lArr; 	&#8656;
-//    ⇑ 	двойная вверх 	&uArr; 	&#8657;
-//    ⇒ 	двойная вправо 	&rArr; 	&#8658;
-//    ⇓ 	двойная вниз 	&dArr; 	&#8659;
-//    ⇔ 	двойная влево-вправо 	&hArr; 	&#8660;
-//    Математические операции
-//    [ 	левая квадратная скобка 		&#91;
-//    ] 	правая квадратная скобка 		&#93;
-//    / 	деление 		&#47;
-//    \ 	разность множеств 		&#92;
-//    ∀ 	для всех 	&forall; 	&#8704;
-//    ∂ 	частичный дифференциал 	&part; 	&#8706;
-//    ∃ 	существует 	&exist; 	&#8707;
-//    ∅ 	пустое множество 	&empty; 	&#8709;
-//    ∇ 	оператор набла = backward difference 	&nabla; 	&#8711;
-//    ∈ 	принадлежит 	&isin; 	&#8712;
-//    ∉ 	не принадлежит 	&notin; 	&#8713;
-//    ∋ 	содержит 	&ni; 	&#8715;
-//    ∏ 	знак произведения 	&prod; 	&#8719;
-//    ∑ 	знак суммы 	&sum; 	&#8721;
-//    − 	минус 	&minus; 	&#8722;
-//    ∗ 	оператор звёздочка 	&lowast; 	&#8727;
-//    √ 	квадратный корень = знак радикала 	&radic; 	&#8730;
-//    ∝ 	пропорционально к 	&prop; 	&#8733;
-//    ∞ 	бесконечность 	&infin; 	&#8734;
-//    ∠ 	угол 	&ang; 	&#8736;
-//    ∧ 	логическое И = wedge 	&and; 	&#8743;
-//    ∨ 	логическое ИЛИ = vee 	&or; 	&#8744;
-//    ∩ 	пересечение = cap 	&cap; 	&#8745;
-//    ∪ 	объединение = cup 	&cup; 	&#8746;
-//    ∫ 	интеграл 	&int; 	&#8747;
-//    ∴ 	следовательно 	&there4; 	&#8756;
-//    ∼ 	оператор тильда = varies with = подобно 	&sim; 	&#8764;
-//    ≅ 	конгруэнтно 	&cong; 	&#8773;
-//    ≈ 	примерно равно 	&asymp; 	&#8776;
-//    ≠ 	не равно 	&ne; 	&#8800;
-//    ≡ 	тождественно равно 	&equiv; 	&#8801;
-//    ≤ 	меньше или равно 	&le; 	&#8804;
-//    ≥ 	больше или равно 	&ge; 	&#8805;
-//    ⊂ 	подмножество 	&sub; 	&#8834;
-//    ⊃ 	включает в себя 	&sup; 	&#8835;
-//    ⊄ 	не включает в себя 	&nsub; 	&#8836;
-//    ⊆ 	подмножество или эквивалентно 	&sube; 	&#8838;
-//    ⊇ 	включает в себя или эквивалентно 	&supe; 	&#8839;
-//    ⊕ 	плюс в круге = прямая сумма 	&oplus; 	&#8853;
-//    ⊗ 	умножение в круге = тензорное произведение 	&otimes; 	&#8855;
-//    ⊥ 	up tack = ортогонально к = перпендикулярно 	&perp; 	&#8869;
-//    ⋅ 	оператор точка 	&sdot; 	&#8901;
-//    Различные технические
-//    ⌈ 	левая скобка округления вверх = apl upstile 	&lceil; 	&#8968;
-//    ⌉ 	правая скобка округления вверх 	&rceil; 	&#8969;
-//    ⌊ 	левая скобка округления вниз = apl downstile 	&lfloor; 	&#8970;
-//    ⌋ 	правая скобка округления вниз 	&rfloor; 	&#8971;
-//    〈 	угловая скобка влево = бра 	&lang; 	&#9001;
-//    〉 	угловая скобка вправо = кет 	&rang; 	&#9002;
-//    Геометрические Фигуры
-//    ◊ 	ромб 	&loz; 	&#9674;
-//    Различные символы
-//    ♠ 	«пики» (картёжные) 	&spades; 	&#9824;
-//    ♣ 	«трефы» 	&clubs; 	&#9827;
-//    ♥ 	«червы» 	&hearts; 	&#9829;
-//    ♦ 	«бубны» 	&diams; 	&#9830;
-//    Latin Extended-A
-//    Œ 	латинская заглавная лигатура OE 	&OElig; 	&#338;
-//    œ 	латинская лигатура oe 	&oelig; 	&#339;
-//    Š 	латинская заглавная S с птичкой 	&Scaron; 	&#352;
-//    š 	латинская s с птичкой 	&scaron; 	&#353;
-//    Ÿ 	латинская заглавная Y с двоеточием 	&Yuml; 	&#376;
-//    Модификаторы букв
-//    ˆ 	акцент перевёрнутая птичка/circumflex accent 	&circ; 	&#710;
-//    ˜ 	малая тильда 	&tilde; 	&#732;
-//    Общая пунктуация
-//        пробел длины N 	&ensp; 	&#8194;
-//        пробел длины M 	&emsp; 	&#8195;
-//        узкий пробел 	&thinsp; 	&#8201;
-//    ‌ 	zero width non-joiner 	&zwnj; 	&#8204;
-//    ‍ 	zero width joiner 	&zwj; 	&#8205;
-//    ‎ 	left-to-right mark 	&lrm; 	&#8206;
-//    ‏ 	right-to-left mark 	&rlm; 	&#8207;
-//    ‑ 	неразрывный (неразделяемый) дефис / non-breaking hyphen 	  	&#8209;
-//    – 	тире длины N 	&ndash; 	&#8211;
-//    — 	тире длины M 	&mdash; 	&#8212;
-//    ‘ 	одиночная левая кавычка 	&lsquo; 	&#8216;
-//    ’ 	одиночная правая кавычка 	&rsquo; 	&#8217;
-//    ‚ 	одиночная low-9/нижняя кавычка 	&sbquo; 	&#8218;
-//    “ 	двойная левая кавычка 	&ldquo; 	&#8220;
-//    ” 	двойная правая кавычка 	&rdquo; 	&#8221;
-//    „ 	двойная low-9/нижняя кавычка 	&bdquo; 	&#8222;
-//    † 	крест/dagger 	&dagger; 	&#8224;
-//    ‡ 	двойной dagger 	&Dagger; 	&#8225;
-//    ‰ 	промилле 	&permil; 	&#8240;
-//    ‹ 	одиночная левая угловая скобка (предложена, но ещё не стандартизирована ISO) 	&lsaquo; 	&#8249;
-//    › 	одиночная правая угловая скобка (предложена, но ещё не стандартизирована ISO) 	&rsaquo; 	&#8250;
-//    € 	евро 	&euro; 	&#8364;
+    { QByteArrayLiteral("circ"), 710 },
+    { QByteArrayLiteral("tilde"), 732 },
+
+    { QByteArrayLiteral("Alpha"), 913 },
+    { QByteArrayLiteral("Beta"), 914 },
+    { QByteArrayLiteral("Gamma"), 915 },
+    { QByteArrayLiteral("Delta"), 916 },
+    { QByteArrayLiteral("Epsilon"), 917 },
+    { QByteArrayLiteral("Zeta"), 918 },
+    { QByteArrayLiteral("Eta"), 919 },
+    { QByteArrayLiteral("Theta"), 920 },
+    { QByteArrayLiteral("Iota"), 921 },
+    { QByteArrayLiteral("Kappa"), 922 },
+    { QByteArrayLiteral("Lambda"), 923 },
+    { QByteArrayLiteral("Mu"), 924 },
+    { QByteArrayLiteral("Nu"), 925 },
+    { QByteArrayLiteral("Xi"), 926 },
+    { QByteArrayLiteral("Omicron"), 927 },
+    { QByteArrayLiteral("Pi"), 928 },
+    { QByteArrayLiteral("Rho"), 929 },
+
+    { QByteArrayLiteral("Sigma"), 931 },
+    { QByteArrayLiteral("Tau"), 932 },
+    { QByteArrayLiteral("Upsilon"), 933 },
+    { QByteArrayLiteral("Phi"), 934 },
+    { QByteArrayLiteral("Chi"), 935 },
+    { QByteArrayLiteral("Psi"), 936 },
+    { QByteArrayLiteral("Omega"), 937 },
+
+    { QByteArrayLiteral("alpha"), 945 },
+    { QByteArrayLiteral("beta"), 946 },
+    { QByteArrayLiteral("gamma"), 947 },
+    { QByteArrayLiteral("delta"), 948 },
+    { QByteArrayLiteral("epsilon"), 949 },
+    { QByteArrayLiteral("zeta"), 950 },
+    { QByteArrayLiteral("eta"), 951 },
+    { QByteArrayLiteral("theta"), 952 },
+    { QByteArrayLiteral("iota"), 953 },
+    { QByteArrayLiteral("kappa"), 954 },
+    { QByteArrayLiteral("lambda"), 955 },
+    { QByteArrayLiteral("mu"), 956 },
+    { QByteArrayLiteral("nu"), 957 },
+    { QByteArrayLiteral("xi"), 958 },
+    { QByteArrayLiteral("omicron"), 959 },
+    { QByteArrayLiteral("pi"), 960 },
+    { QByteArrayLiteral("rho"), 961 },
+    { QByteArrayLiteral("sigmaf"), 962 },
+    { QByteArrayLiteral("sigma"), 963 },
+    { QByteArrayLiteral("tau"), 964 },
+    { QByteArrayLiteral("upsilon"), 965 },
+    { QByteArrayLiteral("phi"), 966 },
+    { QByteArrayLiteral("chi"), 967 },
+    { QByteArrayLiteral("psi"), 968 },
+    { QByteArrayLiteral("omega"), 969 },
+
+    { QByteArrayLiteral("thetasym"), 977 },
+    { QByteArrayLiteral("upsih"), 978 },
+
+    { QByteArrayLiteral("piv"), 982 },
+
+    { QByteArrayLiteral("ensp"), 8194 },
+    { QByteArrayLiteral("emsp"), 8195 },
+    { QByteArrayLiteral("thinsp"), 8201 },
+    { QByteArrayLiteral("zwnj"), 8204 },
+    { QByteArrayLiteral("zwj"), 8205 },
+    { QByteArrayLiteral("lrm"), 8206 },
+    { QByteArrayLiteral("rlm"), 8207 },
+
+    { QByteArrayLiteral("ndash"), 8211 },
+    { QByteArrayLiteral("mdash"), 8212 },
+    { QByteArrayLiteral("lsquo"), 8216 },
+    { QByteArrayLiteral("rsquo"), 8217 },
+    { QByteArrayLiteral("sbquo"), 8218 },
+    { QByteArrayLiteral("ldquo"), 8220 },
+    { QByteArrayLiteral("rdquo"), 8221 },
+    { QByteArrayLiteral("bdquo"), 8222 },
+    { QByteArrayLiteral("dagger"), 8224 },
+    { QByteArrayLiteral("Dagger"), 8225 },
+    { QByteArrayLiteral("bull"), 8226 },
+    { QByteArrayLiteral("hellip"), 8230 },
+    { QByteArrayLiteral("permil"), 8240 },
+    { QByteArrayLiteral("prime"), 8242 },
+    { QByteArrayLiteral("Prime"), 8243 },
+    { QByteArrayLiteral("lsaquo"), 8249 },
+    { QByteArrayLiteral("rsaquo"), 8250 },
+    { QByteArrayLiteral("oline"), 8254 },
+    { QByteArrayLiteral("frasl"), 8260 },
+    { QByteArrayLiteral("euro"), 8364 },
+
+
+    { QByteArrayLiteral("image"), 8465 },
+    { QByteArrayLiteral("weierp"), 8472 },
+    { QByteArrayLiteral("real"), 8476 },
+    { QByteArrayLiteral("trade"), 8482 },
+    { QByteArrayLiteral("alefsym"), 8501 },
+
+    { QByteArrayLiteral("larr"), 8592 },
+    { QByteArrayLiteral("uarr"), 8593 },
+    { QByteArrayLiteral("rarr"), 8594 },
+    { QByteArrayLiteral("darr"), 8595 },
+
+    { QByteArrayLiteral("harr"), 8596 },
+    { QByteArrayLiteral("crarr"), 8629 },
+    { QByteArrayLiteral("lArr"), 8656 },
+    { QByteArrayLiteral("uArr"), 8657 },
+    { QByteArrayLiteral("rArr"), 8658 },
+    { QByteArrayLiteral("dArr"), 8659 },
+    { QByteArrayLiteral("hArr"), 8660 },
+
+    { QByteArrayLiteral("forall"), 8704 },
+    { QByteArrayLiteral("part"), 8706 },
+    { QByteArrayLiteral("exist"), 8707 },
+    { QByteArrayLiteral("empty"), 8709 },
+    { QByteArrayLiteral("nabla"), 8711 },
+    { QByteArrayLiteral("isin"), 8712 },
+    { QByteArrayLiteral("notin"), 8713 },
+    { QByteArrayLiteral("ni"), 8715 },
+    { QByteArrayLiteral("prod"), 8719 },
+    { QByteArrayLiteral("sum"), 8721 },
+    { QByteArrayLiteral("minus"), 8722 },
+    { QByteArrayLiteral("lowast"), 8727 },
+    { QByteArrayLiteral("radic"), 8730 },
+    { QByteArrayLiteral("prop"), 8733 },
+    { QByteArrayLiteral("infin"), 8734 },
+    { QByteArrayLiteral("ang"), 8736 },
+    { QByteArrayLiteral("and"), 8743 },
+    { QByteArrayLiteral("or"), 8744 },
+    { QByteArrayLiteral("cap"), 8745 },
+    { QByteArrayLiteral("cup"), 8746 },
+    { QByteArrayLiteral("int"), 8747 },
+    { QByteArrayLiteral("there4"), 8756 },
+    { QByteArrayLiteral("sim"), 8764 },
+    { QByteArrayLiteral("cong"), 8773 },
+    { QByteArrayLiteral("asymp"), 8776 },
+    { QByteArrayLiteral("ne"), 8800 },
+    { QByteArrayLiteral("equiv"), 8801 },
+    { QByteArrayLiteral("le"), 8804 },
+    { QByteArrayLiteral("ge"), 8805 },
+    { QByteArrayLiteral("sub"), 8834 },
+    { QByteArrayLiteral("sup"), 8835 },
+    { QByteArrayLiteral("nsub"), 8836 },
+    { QByteArrayLiteral("sube"), 8838 },
+    { QByteArrayLiteral("supe"), 8839 },
+    { QByteArrayLiteral("oplus"), 8853 },
+    { QByteArrayLiteral("otimes"), 8855 },
+    { QByteArrayLiteral("perp"), 8869 },
+    { QByteArrayLiteral("sdot"), 8901 },
+
+    { QByteArrayLiteral("lceil"), 8968 },
+    { QByteArrayLiteral("rceil"), 8969 },
+    { QByteArrayLiteral("lfloor"), 8970 },
+    { QByteArrayLiteral("rfloor"), 8971 },
+    { QByteArrayLiteral("lang"), 9001 },
+    { QByteArrayLiteral("rang"), 9002 },
+
+    { QByteArrayLiteral("loz"), 9674 },
+
+    { QByteArrayLiteral("spades"), 9824 },
+    { QByteArrayLiteral("clubs"), 9827 },
+    { QByteArrayLiteral("hearts"), 9829 },
+    { QByteArrayLiteral("diams"), 9830 },
 };
 
 HtmlDecoding::CharsetType HtmlDecoding::charsetType(const QString & ch_name) {
